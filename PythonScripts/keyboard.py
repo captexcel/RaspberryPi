226fc4856f04a9c7742db751c6ae69d6e6ec0b2f
Mr. Ayers <79933840+captexcel@users.noreply.github.com>
2021-03-16 12:39:05 -0400

    Upload keyboard file

---
+++ PythonScripts/keyboard.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Keyboard HotKey
+# Author: CaptExcel
+from pynput import keyboard
+
+def function_1():
+    print('Function 1 activated')
+
+def function_2():
+    print('Function 2 activated')
+
+def function_3():
+    print('Function 3 activated')
+
+print('''
+Press one of the following key combinations:\n
+Function Number 1: [alt]+[ctrl]+r
+Function Number 2: [alt]+[ctrl]+t
+Function Number 3: [alt]+[ctrl]+y
+''')
+with keyboard.GlobalHotKeys({
+        '<alt>+<ctrl>+r': function_1,
+        '<alt>+<ctrl>+t': function_2,
+        '<alt>+<ctrl>+y': function_3}) as h:
+    h.join()
