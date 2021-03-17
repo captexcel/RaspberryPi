b0b356acbc5b32ca812b311f70313dbcef2f9c2c
Mr. Ayers <79933840+captexcel@users.noreply.github.com>
2021-03-17 09:43:36 -0400

    Update mouse_listen.py

--- PythonScripts/mouse_listen.py
+++ PythonScripts/mouse_listen.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 # Mouse Listener
+# Author: CaptExcel
+# Initial Source: 
+# Additional Source(s) as used:
+'''
+# Requirements:
+- pip3 install pynput
+'''
+'''
+# Needed Supplies:
+- N/A 
+'''
+'''
+# Reference Material:
+- N/A 
+'''
+
+# import required libraries
 from pynput import mouse
 
-### Listening Mouse
+# required variables
 
+# main functions
 def on_move(x, y):
 	print('Pointer moved to {0}'.format((x, y)))
 def on_click(x, y, button, pressed):
@@ -14,9 +32,11 @@ def on_click(x, y, button, pressed):
 def on_scroll(x, y, dx, dy):
 	print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up',(x, y)))
 
-while True:
-	with mouse.Listener(
-			on_move=on_move,
-			on_click=on_click,
-			on_scroll=on_scroll) as listener:
-		listener.join()
+# end of file check to see if this file is being opened directly or is being called from another script
+if __name__ == ‘__main__’:
+	while True:
+		with mouse.Listener(
+				on_move=on_move,
+				on_click=on_click,
+				on_scroll=on_scroll) as listener:
+			listener.join()
