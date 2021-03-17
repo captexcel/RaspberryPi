d0ed097b6973a80fac7df9a40b87aba9ee038471
Mr. Ayers <79933840+captexcel@users.noreply.github.com>
2021-03-17 09:25:50 -0400

    Update keyboard.py

--- PythonScripts/keyboard.py
+++ PythonScripts/keyboard.py
@@ -3,11 +3,24 @@
 # Author: CaptExcel
 # Initial Source: https://pypi.org/project/pynput/
 '''
-Requirements:
-pip3 install pynput
+# Requirements:
+- pip3 install pynput
 '''
+'''
+# Needed Supplies:
+- N/A
+'''
+'''
+# Reference Material:
+- N/A
+'''
+
+# import required libraries
 from pynput import keyboard
 
+# required variables
+
+# main functions
 def function_1():
     print('Function 1 activated')
 
@@ -17,14 +30,17 @@ def function_2():
 def function_3():
     print('Function 3 activated')
 
-print('''
-Press one of the following key combinations:\n
-Function Number 1: [alt]+[ctrl]+r
-Function Number 2: [alt]+[ctrl]+t
-Function Number 3: [alt]+[ctrl]+y
-''')
-with keyboard.GlobalHotKeys({
-        '<alt>+<ctrl>+r': function_1,
-        '<alt>+<ctrl>+t': function_2,
-        '<alt>+<ctrl>+y': function_3}) as h:
-    h.join()
+    
+# end of file check to see if this file is being opened directly or is being called from another script
+if __name__ == ‘__main__’:
+    print('''
+    Press one of the following key combinations:\n
+    Function Number 1: [alt]+[ctrl]+r
+    Function Number 2: [alt]+[ctrl]+t
+    Function Number 3: [alt]+[ctrl]+y
+    ''')
+    with keyboard.GlobalHotKeys({
+            '<alt>+<ctrl>+r': function_1,
+            '<alt>+<ctrl>+t': function_2,
+            '<alt>+<ctrl>+y': function_3}) as h:
+        h.join()
