dba848f5af3a8eaeec645cea28794392925253c1
Mr. Ayers <79933840+captexcel@users.noreply.github.com>
2021-03-17 21:29:09 -0400

    Create base.py
    This will act as a starting template for future scripts

---
+++ PythonScripts/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Title: 
+# Description: 
+# Author: CaptExcel
+# Initial Source: 
+# Additional Source(s) as used:
+'''
+# Requirements:
+- N/A
+'''
+'''
+# Needed Supplies:
+- N/A (List of items)
+'''
+'''
+# Reference Material:
+- N/A (ie. Pinout of headers)
+'''
+
+# import required libraries
+
+# define global variables
+
+# main functions
+
+
+# end of file check to see if this file is being opened directly or is being called from another script
+if __name__ == ‘__main__’:
+	# put the appropriate function that should be called if this is the main file is run
